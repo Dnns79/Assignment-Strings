053fa0f03beb39a68e7f212cbf9b8db4bac99684
Dennis79 <79541522+Dnns79@users.noreply.github.com>
2021-03-03 22:35:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_0 = 'Ruud Gullit'
+scorer_1 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = scorer_0 + ' ' + str(goal_0) + ', ' + scorer_1 + ' ' + str(goal_1)
+report = f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute'
+
+player = 'Jan Wouters'
+first_name = player[0:player.find(' ')]
+last_name_len = len(player[player.find(' ')+1:])
+name_short = player[0]+ '. ' + player[player.find(' ')+1:]
+chant = (first_name +'! ') * (len(first_name) -1) + (first_name + '!')
+print(chant)
+good_chant = chant != chant[0:chant.find('!')]
+print(good_chant)
